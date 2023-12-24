a583fa4b3215db24fb6f40f8999063b7e75ff503
Aman-Sirmaur19 <amansirmaur190402@gmail.com>
2023-12-25 04:05:02 +0530

    commit

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,40 @@
+import 'package:daily_meals/widgets/meal_item.dart';
+import 'package:flutter/material.dart';
+
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  // final String categoryId;
+  // final String categoryTitle;
+  //
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+  static const routeName = '/category-meals';
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+    final categoryId = routeArgs['id'];
+    final categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+        appBar: AppBar(
+          // title: Text(categoryTitle),
+          title: Text(categoryTitle!),
+        ),
+        body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return MealItem(
+                title: categoryMeals[index].title,
+                imageUrl: categoryMeals[index].imageUrl,
+                duration: categoryMeals[index].duration,
+                complexity: categoryMeals[index].complexity,
+                affordability: categoryMeals[index].affordability);
+          },
+          itemCount: categoryMeals.length,
+        ));
+  }
+}
